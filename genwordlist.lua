fb657862912e0408cc8389f923ead07e1d53d013
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2022-10-16 22:23:57 +0200

    move lists to a table

--- genwordlist.lua
+++ genwordlist.lua
@@ -1,4 +1,9 @@
 local commands = vim.api.nvim_get_commands({})
 local options = vim.api.nvim_get_all_options_info()
-local helptagss = nil
-local events = nil
+local lists = {
+    "commands"; vim.fn.getcompletion("", "command"),
+    "options"; vim.fn.getcompletion("", "option"),
+    "events"; vim.fn.getcompletion("", "event"),
+    "hilites"; vim.fn.getcompletion("", "highlight"),
+    "filetypes"; vim.fn.getcompletion("", "filetype"),
+}
