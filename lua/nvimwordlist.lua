2ae254f19bc556e29c83b126730904463c5d054e
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2022-10-24 10:36:41 +0200

    Wrap word list generation in a function

--- lua/nvimwordlist.lua
+++ lua/nvimwordlist.lua
@@ -1,6 +1,5 @@
 local M = {}
 local fn = vim.fn
-
 local lists = {
     fn.getcompletion("", "command"),
     fn.getcompletion("", "option"),
@@ -8,17 +7,22 @@ local lists = {
     fn.getcompletion("", "highlight"),
     fn.getcompletion("", "filetype"),
 }
-local words = ""
-for _, list in pairs(lists) do
-    for _, word in pairs(list) do
-        words = words .. word .. "\n"
+
+local update_spell_file = function()
+    local words = ""
+    for _, list in pairs(lists) do
+        for _, word in pairs(list) do
+            words = words .. word .. "\n"
+        end
     end
+    local wordfile = "/tmp/vimwords.lst"
+    local wordlist = io.open(wordfile, "w+")
+    wordlist:write(words)
+    wordlist:close()
+
+    local spelldir = vim.call("spellfile#WritableSpellDir")
+
+    vim.cmd("mkspell " .. spelldir .. "/vim " .. wordfile)
 end
-local wordfile = "/tmp/vimwords.lst"
-local wordlist = io.open(wordfile, "w+")
-wordlist:write(words)
-wordlist:close()
 
-local spelldir = vim.call("spellfile#WritableSpellDir")
-
-vim.cmd("mkspell " .. spelldir .. "/vim " .. wordfile)
+M.update = update_spell_file
