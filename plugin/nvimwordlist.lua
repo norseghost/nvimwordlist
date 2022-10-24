95ac63dd1f54be035cd78e46c4621a8a95fe5a20
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2022-10-24 10:31:13 +0200

    Add plugin initialization file

---
+++ plugin/nvimwordlist.lua
@@ -0,0 +1,15 @@
+-- make sure this file is loaded only once
+if vim.g.loaded_nvimwordlist == 1 then
+    return
+end
+vim.g.loaded_nvimwordlist = 1
+
+-- create any global command that does not depend on user setup
+-- usually it is better to define most commands/mappings in the setup function
+-- Be careful to not overuse this file!
+local nvimwordlist = require("nvimwordlist")
+
+vim.api.nvim_create_user_command(
+    "NvimWordlistUpdate",
+    nvimwordlist.update,
+    {})
